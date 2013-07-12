7633bee000c90eaa70f67f17b80910d2068f3e9c
Ludwig Ortmann <ludwig.ortmann@fu-berlin.de>
2013-07-12 11:14:10 +0200

    osx support for tap

--- cpu/native/cc110x_ng/cc110x_ng_cpu.c
+++ cpu/native/cc110x_ng/cc110x_ng_cpu.c
@@ -25,6 +25,8 @@ static int _native_cc110x_enabled;
 
 void _native_handle_cc110xng_input(void)
 {
+
+    /* TODO: partially move to tap */
     int nread;
     char buf[BUFFER_LENGTH];
     union eth_frame *f;
@@ -37,13 +39,19 @@ void _native_handle_cc110xng_input(void)
     DEBUG("_native_handle_cc110xng_input - read %d bytes\n", nread);
     if (nread > 0) {
         f = (union eth_frame*)&buf;
-        if (ntohs(f->field.header.h_proto) == NATIVE_ETH_PROTO) {
-            nread = nread - ETH_HLEN;
-            memcpy(rx_fifo, buf+ETH_HLEN, nread);
-            status_registers[CC1100_RXBYTES - 0x30] = nread;
-            rx_fifo_idx = 0;
-            DEBUG("_native_handle_cc110xng_input: got %d bytes payload\n", nread);
-            cc110x_gdo2_irq();
+        if (ntohs(f->field.header.ether_type) == NATIVE_ETH_PROTO) {
+            nread = nread - ETHER_HDR_LEN;
+            if ((nread - 1) <= 0) {
+                DEBUG("_native_handle_cc110xng_input: no payload");
+            }
+            else {
+                nread = buf[ETHER_HDR_LEN];
+                memcpy(rx_fifo, buf+ETHER_HDR_LEN+1, nread);
+                status_registers[CC1100_RXBYTES - 0x30] = nread;
+                rx_fifo_idx = 0;
+                printf("_native_handle_cc110xng_input: got %d bytes payload\n", nread);
+                cc110x_gdo2_irq();
+            }
         }
         else {
             DEBUG("ignoring non-native frame\n");
--- cpu/native/tap.c
+++ cpu/native/tap.c
@@ -6,10 +6,19 @@
 #include <err.h>
 #include <sys/ioctl.h>
 #include <fcntl.h>
-#include <net/if.h>
 #include <arpa/inet.h>
+
+#ifdef __MACH__
+#define _POSIX_C_SOURCE
+#include <net/if.h>
+#undef _POSIX_C_SOURCE
+#include <ifaddrs.h>
+#include <net/if_dl.h>
+#else
+#include <net/if.h>
 #include <linux/if_tun.h>
 #include <linux/if_ether.h>
+#endif
 
 #include "cpu-conf.h"
 #include "tap.h"
@@ -17,15 +26,25 @@
 #include "cc110x-internal.h" /* CC1100 constants */
 
 int _native_tap_fd;
-char _native_tap_mac[ETH_ALEN];
+char _native_tap_mac[ETHER_ADDR_LEN];
 
 int send_buf(void)
 {
     uint8_t buf[BUFFER_LENGTH];
     int nsent;
+    uint8_t to_send;
 
-    _native_marshall_ethernet(buf, tx_fifo, status_registers[CC1100_TXBYTES - 0x30]);
-    if ((nsent = write(_native_tap_fd, buf, status_registers[CC1100_TXBYTES - 0x30] + ETH_HLEN)) == -1) {;
+    to_send = status_registers[CC1100_TXBYTES - 0x30];
+    _native_marshall_ethernet(buf, tx_fifo, to_send);
+    to_send += 1;
+
+    if ((ETHER_HDR_LEN + to_send) < ETHERMIN) {
+        printf("padding data! (%d ->", to_send);
+        to_send = ETHERMIN - ETHER_HDR_LEN;
+        printf("%d)\n", to_send);
+    }
+
+    if ((nsent = write(_native_tap_fd, buf, to_send + ETHER_HDR_LEN)) == -1) {;
         warn("write");
         return -1;
     }
@@ -34,13 +53,34 @@ int send_buf(void)
 
 int tap_init(char *name)
 {
+
+#ifdef __MACH__ /* OSX */
+    char clonedev[255] = "/dev/"; /* XXX bad size */
+    strncpy(clonedev+5, name, 250);
+#else /* Linux */
     struct ifreq ifr;
     char *clonedev = "/dev/net/tun";
+#endif
 
+    /* implicitly create the tap interface */
     if ((_native_tap_fd = open(clonedev , O_RDWR)) == -1) {
-        err(EXIT_FAILURE, "open");
+        err(EXIT_FAILURE, "open(%s)", clonedev);
     }
 
+#ifdef __MACH__ /* OSX */
+    struct ifaddrs* iflist;
+    if (getifaddrs(&iflist) == 0) {
+        for (struct ifaddrs *cur = iflist; cur; cur = cur->ifa_next) {
+            if ((cur->ifa_addr->sa_family == AF_LINK) && (strcmp(cur->ifa_name, name) == 0) && cur->ifa_addr) {
+                struct sockaddr_dl* sdl = (struct sockaddr_dl*)cur->ifa_addr;
+                memcpy(_native_tap_mac, LLADDR(sdl), sdl->sdl_alen);
+                break;
+            }
+        }
+
+        freeifaddrs(iflist);
+    }
+#else /* Linux */
     memset(&ifr, 0, sizeof(ifr));
     ifr.ifr_flags = IFF_TAP | IFF_NO_PI;
     strncpy(ifr.ifr_name, name, IFNAMSIZ);
@@ -67,7 +107,8 @@ int tap_init(char *name)
         }
         exit(EXIT_FAILURE);
     }
-    memcpy(_native_tap_mac, ifr.ifr_hwaddr.sa_data, ETH_ALEN);
+    memcpy(_native_tap_mac, ifr.ifr_hwaddr.sa_data, ETHER_ADDR_LEN);
+#endif
 
     puts("RIOT native tap initialized.");
     return _native_tap_fd;
@@ -76,17 +117,18 @@ int tap_init(char *name)
 void _native_marshall_ethernet(uint8_t *framebuf, uint8_t *data, int data_len)
 {
     union eth_frame *f;
-    char addr[ETH_ALEN];
+    char addr[ETHER_ADDR_LEN];
 
     f = (union eth_frame*)framebuf;
     addr[0] = addr[1] = addr[2] = addr[3] = addr[4] = addr[5] = (char)0xFF;
 
-    //memcpy(f->field.header.h_dest, dst, ETH_ALEN);
-    memcpy(f->field.header.h_dest, addr, ETH_ALEN);
-    //memcpy(f->field.header.h_source, src, ETH_ALEN);
-    memcpy(f->field.header.h_source, _native_tap_mac, ETH_ALEN);
-    f->field.header.h_proto = htons(NATIVE_ETH_PROTO);
-    memcpy(f->field.data, data, data_len);
+    //memcpy(f->field.header.ether_dhost, dst, ETHER_ADDR_LEN);
+    memcpy(f->field.header.ether_dhost, addr, ETHER_ADDR_LEN);
+    //memcpy(f->field.header.ether_shost, src, ETHER_ADDR_LEN);
+    memcpy(f->field.header.ether_shost, _native_tap_mac, ETHER_ADDR_LEN);
+    f->field.header.ether_type = htons(NATIVE_ETH_PROTO);
+    memcpy(f->field.data+1, data, data_len);
+    f->field.data[0] = (uint8_t)data_len;
 }
 
 #ifdef TAPTESTBINARY
@@ -107,7 +149,7 @@ int main(int argc, char *argv[])
     char *payld = "abcdefg";
     int data_len = strlen(payld);
     _native_marshall_ethernet(buffer, payld, data_len);
-    if (write(_native_tap_fd, buffer, ETH_HLEN + data_len) == -1) {
+    if (write(_native_tap_fd, buffer, ETHER_HDR_LEN + data_len) == -1) {
         err(EXIT_FAILURE, "write");
     }
 
