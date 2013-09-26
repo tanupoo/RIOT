da6beb924d22b87fb623ded7f04c1e54b2abbd1b
Ludwig Ortmann <ludwig.ortmann@fu-berlin.de>
2013-09-26 16:08:12 +0200

    board socket io support

--- boards/native/include/board_internal.h
+++ boards/native/include/board_internal.h
@@ -1,7 +1,13 @@
 #ifdef MODULE_UART0
 #include <sys/select.h>
 void _native_handle_uart0_input(void);
-void _native_init_uart0(void);
+/**
+ * @brief: initialize uart0
+ *
+ * @param stdiotype: "stdio", "tcp", "udp" io redirection
+ * #param stderrtype: "stdio" or "file"
+ */
+void _native_init_uart0(char *stdiotype, char *stderrtype);
 int _native_set_uart_fds(void);
 #endif
 
