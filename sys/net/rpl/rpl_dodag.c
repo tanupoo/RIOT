3872d9d534aa7dbf2e96d075dcf27d08d9823680
Oleg Hahm <oleg@hobbykeller.org>
2013-11-22 23:59:33 +0100

    debug output when joining a dodag

--- sys/net/rpl/rpl_dodag.c
+++ sys/net/rpl/rpl_dodag.c
@@ -24,6 +24,9 @@
 #include "rpl.h"
 
 #define ENABLE_DEBUG (0)
+#if ENABLE_DEBUG
+char addr_str[IPV6_MAX_ADDR_STR_LEN];
+#endif
 #include "debug.h"
 
 rpl_instance_t instances[RPL_MAX_INSTANCES];
@@ -367,6 +370,14 @@ void rpl_join_dodag(rpl_dodag_t *dodag, ipv6_addr_t *parent, uint16_t parent_ran
     my_dodag->my_rank = dodag->of->calc_rank(preferred_parent, dodag->my_rank);
     my_dodag->dao_seq = RPL_COUNTER_INIT;
     my_dodag->min_rank = my_dodag->my_rank;
+    DEBUG("Joint DODAG:\n");
+    DEBUG("\tMOP:\t%02X\n", my_dodag->mop);
+    DEBUG("\tminhoprankincrease :\t%04X\n", my_dodag->minhoprankincrease);
+    DEBUG("\tdefault_lifetime:\t%02X\n", my_dodag->default_lifetime);
+    DEBUG("\tgrounded:\t%02X\n", my_dodag->grounded);
+    DEBUG("\tmy_preferred_parent:\t%s\n", ipv6_addr_to_str(addr_str, &my_dodag->my_preferred_parent->addr));
+    DEBUG("\tmy_preferred_parent rank\t%02X\n", my_dodag->my_preferred_parent->rank);
+    DEBUG("\tmy_preferred_parent lifetime\t%04X\n", my_dodag->my_preferred_parent->lifetime);
 
 #ifdef SAFEST_DEMO
         printf("p_s: ID sn%u selected ID sn%u as parent #color15\n",
