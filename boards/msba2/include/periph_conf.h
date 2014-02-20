44788637976adcb23c963892bb77a9ef8f9911a8
Hauke Petersen <hauke.petersen@fu-berlin.de>
2014-02-20 15:53:49 +0100

    boards: added periph_conf file to msba2

---
+++ boards/msba2/include/periph_conf.h
@@ -0,0 +1,33 @@
+/*
+ * Copyright (C) 2014 Freie Universität Berlin
+ *
+ * This file is subject to the terms and conditions of the LGPLv2 License.
+ * See the file LICENSE in the top level directory for more details.
+ */
+
+/**
+ * @ingroup     board_msba2
+ * @{
+ *
+ * @file        pwm.h
+ * @brief       MSBA2 peripheral configuration
+ *
+ * @author      Hauke Petersen <hauke.petersen@fu-berlin.de>
+ */
+
+#ifndef __PERIPH_CONF_H
+#define __PERIPH_CONF_H
+
+
+/**
+ * @brief PWM device and pinout configuration
+ */
+#define PWM_NUMOF           (1)
+#define PWM_0_EN            (1)
+
+#define PWM_0_PORT
+#define PWM_0_PINS
+
+
+#endif /* __PERIPH_CONF_H */
+/** @} */
