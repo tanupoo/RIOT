8b9bc9e63adc889fe707a04a71ccec3711c3d31f
Oleg Hahm <oleg@hobbykeller.org>
2014-08-04 00:09:51 +0200

    cpu: initial import of support for stm32l1 MCUs

---
+++ cpu/stm32l1/include/cpu-conf.h
@@ -0,0 +1,75 @@
+/*
+ * Copyright (C) 2013 INRIA
+ * Copyright (C) 2014 Freie Universität Berlin
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser General
+ * Public License v2.1. See the file LICENSE in the top level directory for more
+ * details.
+ */
+
+/**
+ * @defgroup        cpu_stm32l1 STM32L1
+ * @addtogroup      cpu
+ * @brief           CPU specific implementations for the STM32L1
+ * @{
+ *
+ * @file
+ * @brief           Implementation specific CPU configuration options
+ *
+ * @author          Alaeddine Weslati <alaeddine.weslati@intia.fr>
+ * @author          Hauke Petersen <hauke.petersen@fu-berlin.de>
+ */
+
+#ifndef CPUCONF_H_
+#define CPUCONF_H_
+
+#include "stm32l1xx.h"
+
+/**
+ * @name Kernel configuration
+ *
+ * TODO: measure and adjust for the cortex-m3
+ * @{
+ */
+#define KERNEL_CONF_STACKSIZE_PRINTF    (1024)
+
+#ifndef KERNEL_CONF_STACKSIZE_DEFAULT
+#define KERNEL_CONF_STACKSIZE_DEFAULT   (1024)
+#endif
+
+#define KERNEL_CONF_STACKSIZE_IDLE      (256)
+/** @} */
+
+/**
+ * @name UART0 buffer size definition for compatibility reasons
+ *
+ * TODO: remove once the remodeling of the uart0 driver is done
+ * @{
+ */
+#ifndef UART0_BUFSIZE
+#define UART0_BUFSIZE                   (128)
+#endif
+/** @} */
+
+/**
+ * @name Macro for reading CPU_ID
+ */
+#define GET_CPU_ID(id)          memcpy(&id, (void *)(0x1ffff7e8), CPU_ID_LEN)
+
+/**
+ * @name Definition of different panic modes
+ */
+typedef enum {
+    HARD_FAULT,
+    WATCHDOG,
+    BUS_FAULT,
+    USAGE_FAULT,
+    DUMMY_HANDLER
+} panic_t;
+
+void cpu_clock_scale(uint32_t source, uint32_t target, uint32_t *prescale);
+
+#define TRANSCEIVER_BUFFER_SIZE (3)
+
+#endif /* __CPU_CONF_H */
+/** @} */
