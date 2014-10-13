8cc3a075d94c0de3cce5ff5b8a38de07a7b9ef4b
Lotte Steenbrink <lotte.steenbrink@fu-berlin.de>
2014-10-13 08:30:59 -0700

    add license header to aodvv2/aodvv2.h

--- sys/net/include/aodvv2/aodvv2.h
+++ sys/net/include/aodvv2/aodvv2.h
@@ -1,3 +1,22 @@
+/*
+ * Copyright (C) 2014 Freie Universität Berlin
+ * Copyright (C) 2014 Lotte Steenbrink <lotte.steenbrink@fu-berlin.de>
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+/**
+ * @ingroup     aodvv2
+ * @{
+ *
+ * @file        aodvv2/aodvv2.h
+ * @brief       the aodvv2 routing protocol
+ *
+ * @author      Lotte Steenbrink <lotte.steenbrink@fu-berlin.de>
+ */
+
 #include "common/netaddr.h"
 #include "rfc5444/rfc5444_print.h"
 
